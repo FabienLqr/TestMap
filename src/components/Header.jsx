0736e74290a359ee2bdb81c2286078f4e7bc0724
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-05-31 22:35:10 +0200

    Header done: simple style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
+import logo from "../assets/logo.png";
+
 function Header() {
   return (
-    <>
-      <h1>C&apos;est le Header</h1>
-    </>
+    <header>
+      <img src={logo} alt="logo" className="imgLogo" />
+      <h1 className="headerTitle">
+        Test de différentes façons de rendre une map en JS
+      </h1>
+      <img src={logo} alt="logo" className="imgLogo" />
+    </header>
   );
 }
 
