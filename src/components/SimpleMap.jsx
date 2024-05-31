ac4b133f14128573e4616ec1b4fde5364a2e8507
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-06-01 00:14:20 +0200

    test fix, fonctionne pas je reprend demain

--- src/components/SimpleMap.jsx
+++ src/components/SimpleMap.jsx
@@ -6,7 +6,7 @@ import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 export default function SimpleMap() {
   return (
     <ComposableMap>
-      <Geographies geography="../../public/custom.json">
+      <Geographies geography="/custom.geo.json">
         {({ geographies }) =>
           geographies.map((geo) => (
             <Geography key={geo.rsmKey} geography={geo} />
