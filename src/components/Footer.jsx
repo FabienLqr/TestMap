920d5547c0141cbc3b653560e2437aab09518abd
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-05-31 23:04:51 +0200

    Footer done with style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,25 @@
+import logoGithub from "../assets/github.svg";
+import logoLinkedin from "../assets/linkedin.svg";
+
 function Footer() {
   return (
     <>
-      <h1>C&apos;est le Footer</h1>
+      <footer>
+        <p>Fabien</p>
+        <a href="https://github.com/FabienLqr" className="hrefGithub">
+          <img className="logoGithub" src={logoGithub} alt="logo github"></img>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/fabien-laquerriere/"
+          className="hrefGithub"
+        >
+          <img
+            className="logoGithub"
+            src={logoLinkedin}
+            alt="logo github"
+          ></img>
+        </a>
+      </footer>
     </>
   );
 }
