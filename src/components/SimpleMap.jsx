54491085ffb51eaea958220f665ae90a83f6ca72
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-06-01 22:33:40 +0200

    desespoire

--- src/components/SimpleMap.jsx
+++ src/components/SimpleMap.jsx
@@ -1,18 +1,3 @@
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
-
-// const geoUrl =
-//  "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json";
-
 export default function SimpleMap() {
-  return (
-    <ComposableMap>
-      <Geographies geography="/crashtest.geo.json">
-        {({ geographies }) =>
-          geographies.map((geo) => (
-            <Geography key={geo.rsmKey} geography={geo} />
-          ))
-        }
-      </Geographies>
-    </ComposableMap>
-  );
+  return <>coucou</>;
 }
