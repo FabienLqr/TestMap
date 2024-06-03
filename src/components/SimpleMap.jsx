fb21e1899a630c0a636add678f56701a4f466146
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-06-03 12:17:06 +0200

    leaflet affiche une map

--- src/components/SimpleMap.jsx
+++ src/components/SimpleMap.jsx
@@ -1,3 +1,3 @@
 export default function SimpleMap() {
-  return <>coucou</>;
+  return <p>React Simple map ne fonctionne pas.</p>;
 }
--- src/pages/Leaflet.jsx
+++ src/pages/Leaflet.jsx
@@ -1,5 +1,31 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
 function Leaflet() {
-  return <p>temp</p>;
+  return (
+    <>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        style={{ height: "80", width: "100%" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>Un Popup pour toute information supplémentaire.</Popup>
+        </Marker>
+      </MapContainer>
+    </>
+  );
 }
 
 export default Leaflet;
+
+/*
+
+var carte = L.map('carte').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© Open
+
+*/
